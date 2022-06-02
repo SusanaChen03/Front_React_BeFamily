68f708eaf69dae7d57ec0b152f62e63f73748257
SusanaChen03 <you@example.com>
2022-06-02 11:24:27 +0200

    Create Container Register.js and css

---
+++ src/containers/Register/Register.js
@@ -0,0 +1,36 @@
+import "./Register.css";
+import Onboarding2 from "../../components/Onboarding2/Onboarding2";
+
+const Register = () => {
+  return (
+    <div>
+      <Onboarding2 />
+      <hr />
+      <div className="welcome">
+        <h1>REGÍSTRATE Y EMPIEZA LA AVENTURA</h1>
+      </div>
+      <form>
+        <label htmlFor="familyName">
+          Destaca tu familia! Poneos un nombre chulo
+        </label>
+        <input type="text" id="familyName" name="familyName" />
+
+        <label htmlFor="name">¿Cómo te llamas?</label>
+        <input type="text" id="name" name="name" />
+
+        <label htmlFor="birthday">Fecha de nacimiento</label>
+        <input type="date" id="birthday" name="birthday" />
+
+        <label htmlFor="email">Tu dirección de correo preferida</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="password">Contraseña</label>
+        <input type="password" id="password" name="password" />
+
+        <input type="submit" value="CONTINUAR" className="sendButton"/>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
