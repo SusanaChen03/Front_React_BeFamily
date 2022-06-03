4d01c3882564f482f5e63d54d7bd10ee5fe2e537
SusanaChen03 <you@example.com>
2022-06-03 17:08:47 +0200

    function send the register form

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -1,15 +1,44 @@
 import "./Register.css";
-import Onboarding2 from "../../components/Onboarding2/Onboarding2";
+
 
 const Register = () => {
+
+  const formSubmit = async (e) => {
+
+    e.preventDefault();
+    try {
+      const formData = {
+        familyName: e.target[0].value,
+        name: e.target[1].value,
+        birthday: e.target[2].value,
+        email: e.target[3].value,
+        password: e.target[4].value,
+      };
+      let registerUser = await fetch( "http://localhost:8000/api/register", 
+        {
+          method:"POST",
+          body: JSON.stringify(formData),
+          headers:{
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await registerUser.json();
+      if(!data){
+        console.log('error no have data')
+      }
+ 
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <Onboarding2 />
-      <hr />
       <div className="welcome">
         <h1>REGÍSTRATE Y EMPIEZA LA AVENTURA</h1>
       </div>
-      <form>
+      <form onSubmit={(e) => formSubmit(e)}>
         <label htmlFor="familyName">
           Destaca tu familia! Poneos un nombre chulo
         </label>
@@ -28,7 +57,11 @@ const Register = () => {
         <input type="password" id="password" name="password" />
 
         <input type="submit" value="CONTINUAR" className="sendButton"/>
-        <a>Ya tengo cuenta</a>
+
+        <div className="linkAccount">
+        <a className="haveAccount">Ya tengo cuenta</a>
+        </div>
+
       </form>
     </div>
   );
