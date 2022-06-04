22558690c316e1fabd4d29fa4c4476e2622022b8
SusanaChen03 <you@example.com>
2022-06-04 14:51:29 +0200

    create folder Store and Subfolders

---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import { createStore } from "redux";
+import { HIDDEN_POPUP, USER_LOGGED, SHOW_POPUP,USER_LOGOUT} from "./typesVar.js";
+
+
+import {
+
+} from './typesVar.js';
+
+
+const initialState = {
+
+};
+
+
+const reducer = (state = initialState, action) => {
+
+};
+
+
+export default createStore(reducer)
+
