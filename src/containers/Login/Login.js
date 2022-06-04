9354653d9b7b8a982d7b4ca99f3ba2ab818afcd0
SusanaChen03 <you@example.com>
2022-06-04 14:50:44 +0200

    Form send Login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,19 +1,55 @@
 import Header from "../Header/Header";
 import "./Login.css";
+import {URL_LOCAL} from "../../store/typesVar.js";
+
+
 
 const Login = () => {
+
+  const formSubmit = async (e) => {
+
+    e.preventDefault();
+    try {
+      const formData = {
+        email: e.target[0].value,
+        password: e.target[1].value,
+      };
+      console.log(formData);
+      let loginUser = await fetch( URL_LOCAL +"/login", 
+        {
+          method:"POST",
+          
+          body: JSON.stringify(formData),
+          headers:{
+            "Content-Type": "application/json"
+          },
+        }
+      );
+      const data = await loginUser.json();
+      if(!data){
+        console.log('error no have data')
+      }
+      console.log('Correctp_'+data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Header />
       <div className="loginContainer">
-        <h1>BIENVENIDOS DE NUEVO</h1>
+        <h1>BIENVENIDOS!</h1>
 
-        <form>
+        <form onSubmit={(e) => formSubmit(e)}>
           <label htmlFor="email">Tu dirección de correo</label>
           <input type="email" id="email" name="email" />
 
           <label htmlFor="password">Contraseña</label>
           <input type="password" id="password" name="password" />
+
+          <input type="submit" value="CONTINUAR" className="sendButton"/>
+
         </form>
       </div>
     </div>
