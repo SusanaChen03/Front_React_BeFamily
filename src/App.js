37b5d897dcd6ccad777346e020e16f1cd071c0f3
SusanaChen03 <you@example.com>
2022-06-05 11:10:48 +0200

    APP.js for test screens

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 
 import './App.css'
-//import Onboarding from './components/Onboarding/Onboarding';
-//import Onboarding2 from './components/Onboarding2/Onboarding2';
+import './css/bootstrap.min.css';
+import boots from 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+
+import Onboarding from './components/Onboarding/Onboarding';
 import Register from './containers/Register/Register';
+import Header from './components/Header/Header';
+import Login from './containers/Login/Login';
+import RegisterMember from './containers/RegisterMember/RegisterMember';
 
 function App() {
+
   return (
-    <Register/>
-   
+    <div className='app'>
+      <Header/>
+      <RegisterMember/>
+    
+    </div>
 
 
   );
