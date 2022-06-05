23a1443707caa5206362cbb1721e5431e9d8cb2e
SusanaChen03 <you@example.com>
2022-06-05 16:50:40 +0200

    Container ListChallengers HTML

---
+++ src/containers/ListChallengers/ListChallengers.js
@@ -0,0 +1,91 @@
+import './ListChallengers.css';
+import {IoAddCircleOutline } from 'react-icons/io5'
+
+const ListChallengers = () =>{
+    return(
+        <div>
+            <h1>Retos de la Familia </h1>
+
+            <div className='profileChallengers'>
+                <div class="card">
+                    <img src="https://anpeandalucia.es/imageFile.php?link=portfolio/1/aqualand_t1559905111a.jpg" 
+                    class="card-img-top" alt="Aqualand" />
+
+                    <div class="card-body">
+                        <h5 class="card-title">Vacaciones de Verano</h5>
+                        <p class="card-text">2 dias en el parque Acuatico full equip</p>
+                        <p class="card-text">Una mes haciendo la cama</p>
+                        <a href="#" class="btn btn-primary">Ir a Reto</a>
+                    </div>
+                </div>
+                
+                 
+                <div class="card">
+                    <img src="https://anpeandalucia.es/imageFile.php?link=portfolio/1/aqualand_t1559905111a.jpg" 
+                    class="card-img-top" alt="Aqualand" />
+
+                    <div class="card-body">
+                        <h5 class="card-title">Vacaciones de Verano</h5>
+                        <p class="card-text">2 dias en el parque Acuatico full equip</p>
+                        <p class="card-text">Una mes haciendo la cama</p>
+                        <a href="#" class="btn btn-primary">Ir a Reto</a>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <img src="https://anpeandalucia.es/imageFile.php?link=portfolio/1/aqualand_t1559905111a.jpg" 
+                    class="card-img-top" alt="Aqualand" />
+
+                    <div class="card-body">
+                        <h5 class="card-title">Vacaciones de Verano</h5>
+                        <p class="card-text">2 dias en el parque Acuatico full equip</p>
+                        <p class="card-text">Una mes haciendo la cama</p>
+                        <a href="#" class="btn btn-primary">Ir a Reto</a>
+                    </div>
+                </div>
+
+                
+                <div class="card">
+                    <img src="https://anpeandalucia.es/imageFile.php?link=portfolio/1/aqualand_t1559905111a.jpg" 
+                    class="card-img-top" alt="Aqualand" />
+
+                    <div class="card-body">
+                        <h5 class="card-title">Vacaciones de Verano</h5>
+                        <p class="card-text">2 dias en el parque Acuatico full equip</p>
+                        <p class="card-text">Una mes haciendo la cama</p>
+                        <a href="#" class="btn btn-primary">Ir a Reto</a>
+                    </div>
+                </div>
+
+                
+                <div class="card">
+                    <img src="https://anpeandalucia.es/imageFile.php?link=portfolio/1/aqualand_t1559905111a.jpg" 
+                    class="card-img-top" alt="Aqualand" />
+
+                    <div class="card-body">
+                        <h5 class="card-title">Vacaciones de Verano</h5>
+                        <p class="card-text">2 dias en el parque Acuatico full equipeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</p>
+                        <p class="card-text">Una mes haciendo la cama</p>
+                        <a href="#" class="btn btn-primary">Ir a Reto</a>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <img src="https://anpeandalucia.es/imageFile.php?link=portfolio/1/aqualand_t1559905111a.jpg" 
+                    class="card-img-top" alt="Aqualand" />
+
+                    <div class="card-body">
+                        <h5 class="card-title">Añade un nuevo Reto</h5>
+                       
+                    <h1 class="iconAdd"><IoAddCircleOutline/> </h1> 
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    )
+};
+
+
+export default ListChallengers;
