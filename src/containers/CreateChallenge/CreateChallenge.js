d66ca581bfef014e772ba478f478eca9216d69c1
SusanaChen03 <you@example.com>
2022-06-05 12:47:08 +0200

    add  images for select rewards

--- src/containers/CreateChallenge/CreateChallenge.js
+++ src/containers/CreateChallenge/CreateChallenge.js
@@ -1,42 +1,77 @@
-import './CreateChallenge.css';
+import "./CreateChallenge.css";
+import cinema from "../../IMG/iconCinema.png";
+import teamPark from "../../IMG/iconTeamPark.png";
+import bike from "../../IMG/bike.png";
+import aquarium from "../../IMG/aquarium.png";
 
 const CreateChallenge = () => {
-    return(
-        <div>
+  return (
+    <div>
+      <div>
+        <h1>Creación de un nuevo reto</h1>
+
+        <form>
+          <div>
+            <label htmlFor="whoMember"> ¿Quienes van a participar?</label>
+
             <div>
-                <h1>Creación de un nuevo reto</h1>
-
-                <form>
-                    <div>
-
-                    <label htmlFor="whoMember"> ¿Quienes van a participar?</label>
-
-                    <div>
-                    <button type='submit' className='names' >name1</button>
-                    <button type='submit' className='names'>name2</button>
-                    <button type='submit' className='names'>name3</button>
-                    </div>
-                  
-                    <label htmlFor="challengeName">Nombre del reto</label>
-                    <input type="text" id="challengeName" name="challengeName" />
-                   
-                    <label htmlFor="repetition">Duración</label>
-                    <input type="text" id="repetition" name="repetition" placeholder="numero de repeticiones"></input>
-
-                    <label htmlFor="reward">Recompensa</label>
-                    <input type="text" id="reward" name="reward"></input>
-                    </div>
-
-                    <input type="submit" value="CREAR RETO" className="sendButton"/>
-
-
-                </form>
-
+              <button type="submit" className="names">
+                name1
+              </button>
+              <button type="submit" className="names">
+                name2
+              </button>
+              <button type="submit" className="names">
+                name3
+              </button>
             </div>
-            
 
-        </div>
-    )
+            <label htmlFor="challengeName">Nombre del reto</label>
+            <input type="text" id="challengeName" name="challengeName" />
+
+            <label htmlFor="repetition">Duración</label>
+            <input
+              type="text"
+              id="repetition"
+              name="repetition"
+              placeholder="numero de repeticiones"
+            ></input>
+
+            <label htmlFor="reward">Recompensa</label>
+            <div className="selectReward">
+              <div className="selected">
+                <img src={cinema} alt="iconCinema" />
+                <p>Entradas de cine</p>
+              </div>
+              <div className="selected">
+                <img src={teamPark} alt="iconCinema" />
+                <p>Parque de atracciones</p>
+              </div>
+              <div className="selected">
+                <img src={bike} alt="iconCinema" />
+                <p>Una bicicleta</p>
+              </div>
+              <div className="selected">
+                <img src={aquarium} alt="iconCinema" />
+                <p>Acuario Atlantis</p>
+              </div>
+            </div>
+
+            <label htmlFor="writeYou">Escribe uno tú</label>
+
+            <input
+              type="text"
+              id="reward"
+              name="reward"
+              placeholder="opcional"
+            ></input>
+          </div>
+
+          <input type="submit" value="CREAR RETO" className="sendButton" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CreateChallenge;
