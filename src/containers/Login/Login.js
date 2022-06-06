461f10f833b43506f60b18ff9caa1a98b38e0203
SusanaChen03 <you@example.com>
2022-06-06 16:17:34 +0200

    Funtionality Login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,39 +1,70 @@
-import Header from "../../components/Header/Header.js"
+import Header from "../../components/Header/Header.js";
 import "./Login.css";
-import {URL_LOCAL} from "../../store/typesVar.js";
+import { SHOW_POPUP, URL_LOCAL, USER_LOGGED, HIDDEN_POPUP } from "../../store/typesVar.js";
 import { useNavigate } from "react-router-dom";
-
-
-
+import { useDispatch } from "react-redux";
+import actionCreator from "../../store/actionTypes.js";
 
 const Login = () => {
-
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
 
   const formSubmit = async (e) => {
-
     e.preventDefault();
     try {
       const formData = {
         email: e.target[0].value,
         password: e.target[1].value,
       };
-      console.log(formData);
-      let loginUser = await fetch( URL_LOCAL +"/login", 
-        {
-          method:"POST",
-          
-          body: JSON.stringify(formData),
-          headers:{
-            "Content-Type": "application/json"
-          },
-        }
-      );
+
+      let loginUser = await fetch(URL_LOCAL + "/login", {
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
       const data = await loginUser.json();
-      if (data) {
-        alert("User Logged");
-        navigate("/home");
+     
+      if (data.success === true) {
+        sessionStorage.setItem("token", data.token);
+        sessionStorage.setItem("id", data.user.id);
+        sessionStorage.setItem("rol", data.user.rol);
+        sessionStorage.setItem("name", data.user.name);
+        sessionStorage.setItem("logged", true);
+
+        if (sessionStorage.getItem("rol") === "admin") {
+          dispatch(
+            actionCreator(USER_LOGGED, {
+              token: data.token,
+              id: data.user.id,
+              name: data.user.name,
+              role: data.user.role,
+            })
+          );
+          dispatch( actionCreator(SHOW_POPUP, "Se a iniciado correctamente. Bienvenid@ Admin"));
+
+          setTimeout(() => dispatch(actionCreator(HIDDEN_POPUP)), 2000);
+          navigate("/home");
+        } else {
+          dispatch(
+            actionCreator(USER_LOGGED, {
+              token: data.token,
+              id: data.user.id,
+              name: data.user.name,
+              role: data.user.role
+            })
+          );
+          dispatch(actionCreator(SHOW_POPUP, "Se a iniciado correctamente. Bienvenido" + {name: data.name}));
+          console.log(
+            "Se a iniciado correctamente. Bienvenido" + { name: data.name }
+          );
+          setTimeout(() => dispatch(actionCreator(HIDDEN_POPUP)), 2000);
+          navigate("/home");
+        }
+      } else {
+        alert("Usuario y/o contraseña incorrecto.");
       }
     } catch (error) {
       alert("Failed user login" + error);
@@ -43,7 +74,6 @@ const Login = () => {
 
   return (
     <div>
-
       <div className="loginContainer">
         <h1>BIENVENIDOS!</h1>
 
@@ -54,8 +84,7 @@ const Login = () => {
           <label htmlFor="password">Contraseña</label>
           <input type="password" id="password" name="password" />
 
-          <input type="submit" value="CONTINUAR" className="sendButton"/>
-
+          <input type="submit" value="CONTINUAR" className="sendButton" />
         </form>
       </div>
     </div>
