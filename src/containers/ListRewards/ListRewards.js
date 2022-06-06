a9772a8b9c41096e44ded4bfb82d9ce90f4e144e
SusanaChen03 <you@example.com>
2022-06-06 19:29:38 +0200

    Move html Card to RewardCard

--- src/containers/ListRewards/ListRewards.js
+++ src/containers/ListRewards/ListRewards.js
@@ -1,67 +1,35 @@
 import "./ListRewards.css";
-import { IoAddCircleOutline } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { SHOW_POPUP, URL_LOCAL, HIDDEN_POPUP } from "../../store/typesVar.js";
+import RewardCard from "../../components/RewardCard/RewardCard";
 
 const ListRewards = () => {
+  const [listReward, setListReward] = useState([]);
+
+  const rewardList = async () => {
+    const results = await fetch(URL_LOCAL + "/rewards", {
+      method: "GET",
+    });
+
+    const dataRewards = await results.json();
+    setListReward(dataRewards);
+  };
+
+  
+  useEffect(() => {
+    try {
+      rewardList();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <div>
       <h1>Lista de recompensas</h1>
-
-      <div className="profileStyles">
-        <div class="card">
-          <img
-            src="https://areajugones.sport.es/wp-content/uploads/2022/01/eternals-2.jpg"
-            class="card-img-top"
-            alt="cinema"  />
-         
-          <div class="card-body">
-            <h5 class="card-title">Entradas de cine</h5>
-            <p class="card-text">Cine para la película ETERNALS</p>
-          </div>
-        </div>
-
-        <div class="card">
-          <img
-            src="https://i.ytimg.com/vi/tKa3mSgpRi8/maxresdefault.jpg"
-            class="card-img-top"
-            alt="cinema"  />
-         
-          <div class="card-body">
-            <h5 class="card-title">Entradas al parque de atracciones</h5>
-            <p class="card-text">Entradas al parque de atracciones</p>
-          </div>
-        </div>
-
-        <div class="card">
-          <img
-            src="https://i.ytimg.com/vi/tKa3mSgpRi8/maxresdefault.jpg"
-            class="card-img-top"
-            alt="cinema"  />
-         
-          <div class="card-body">
-            <h5 class="card-title">Entradas al parque de atracciones</h5>
-            <p class="card-text">Entradas al parque de atracciones</p>
-          </div>
-        </div>
-
-        <div class="card">
-          <img
-            src="https://anpeandalucia.es/imageFile.php?link=portfolio/1/aqualand_t1559905111a.jpg"
-            class="card-img-top"
-            alt="Aqualand"
-          />
-
-          <div class="card-body">
-            <h5 class="card-title">Añade una nueva recompensa</h5>
-
-            <h1 class="iconAdd">
-              <Link to='/createReward'>
-              <IoAddCircleOutline />{""}
-              </Link>
-            </h1>
-          </div>
-        </div>
-      </div>
+      {listReward.map(()=>{
+        return <RewardCard/>
+      })};
     </div>
   );
 };
