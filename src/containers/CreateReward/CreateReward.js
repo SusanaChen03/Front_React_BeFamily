bdce9c503cb2062f5dbb521857444b8e78bc5c9b
SusanaChen03 <you@example.com>
2022-06-06 18:38:48 +0200

    Funcionality CreateReward

--- src/containers/CreateReward/CreateReward.js
+++ src/containers/CreateReward/CreateReward.js
@@ -1,13 +1,12 @@
-import { useNavigate } from "react-router-dom";
 import "./CreateReward.css";
+import { useNavigate } from "react-router-dom";
+import { SHOW_POPUP, URL_LOCAL, HIDDEN_POPUP } from "../../store/typesVar.js";
+import actionCreator from "../../store/actionTypes";
+import { useDispatch } from "react-redux";
 
 const CreateReward = () => {
-    const navigate = useNavigate();
-
-
-    const buttonHandler = () =>{
-        navigate('/createChallenge')
-;    }
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const formSubmit = async (e) => {
     e.preventDefault();
@@ -16,26 +15,38 @@ const CreateReward = () => {
         name: e.target[0].value,
         image: e.target[1].value,
         description: e.target[2].value,
-        challenge_id: e.target[3].value,
       };
 
-      let postReward = await fetch("http://localhost:8000/api/reward", {
+      let postReward = await fetch(URL_LOCAL + "/reward", {
         method: "POST",
         body: JSON.stringify(formData),
         headers: {
           "Content-Type": "application/json",
+          Authorization: "Bearer " + sessionStorage.getItem("token"),
         },
       });
 
       const rewardData = await postReward.json();
 
-      if (rewardData) {
-        alert("The new reward is generated");      }
+      if (rewardData.success === true) {
+        dispatch(
+          actionCreator(SHOW_POPUP, "Se ha creado una nueva recompensa")
+        );
+        alert("The new reward is generated");
+        navigate("/listRewards");
+      } else {
+        alert("reward creation failed:" + rewardData.data);
+      }
     } catch (error) {
+      dispatch(
+        actionCreator(SHOW_POPUP, "No se ha podido crear la recompensa")
+      );
+
       alert("reward creation failed" + error);
       console.log(error);
     }
   };
+
   return (
     <div>
       <div>
@@ -61,7 +72,6 @@ const CreateReward = () => {
             type="submit"
             value="CREAR RECOMPENSA"
             className="sendButton"
-            onClick={buttonHandler}
           />
         </form>
       </div>
