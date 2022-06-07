bf7d136695c3e2440056f854103119042d5b6296
SusanaChen03 <you@example.com>
2022-06-07 11:11:19 +0200

    GET challenge info in

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,37 @@
 import './Home.css';
+import MiniCardChallenger from '../../components/MiniCardChallenger/MiniCardChallenger';
+import { useEffect, useState } from 'react';
+import { SHOW_POPUP, URL_LOCAL, HIDDEN_POPUP } from "../../store/typesVar.js";
+
+
 
 const Home = () =>{
-    return(
-        <div>
 
+    const [card, setCard] = useState([]);
+
+    const cardList = async () =>{
+        const cardsResult = await fetch(URL_LOCAL + "/challenges", {
+            method: "GET",
+        });
+        
+        const dataCard = await cardsResult.json();
+        setCard(dataCard);
+    }
+
+    useEffect(() => {
+        try {
+            cardList();
+        } catch (error) {
+          console.log(error);
+        }
+      }, []);
+    
+
+
+
+    return(
+        <div className='ContainerChallengers'>
+            <MiniCardChallenger/>
         </div>
     )
 };
