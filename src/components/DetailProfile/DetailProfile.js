57b7a5aea078d28d10212222da0c7b79237cbb5b
SusanaChen03 <you@example.com>
2022-06-09 14:31:06 +0200

    Create detail profile

---
+++ src/components/DetailProfile/DetailProfile.js
@@ -0,0 +1,31 @@
+import './DetailProfile.css';
+
+const DetailProfile = (objUser) =>{
+    return(
+
+        <div className='contentProfile'>
+        <div className="contentUser">
+          <label className="key">NOMBRE:</label>
+          <label className="value"> {objUser.user.name} </label>
+        </div>
+
+        <div className="contentUser">
+          <label className="key">EMAIL:</label>
+          <label className="value"> {objUser.user.email} </label>
+        </div>
+
+        <div className="contentUser">
+          <label className="key">NOMBRE DE LA FAMILIA:</label>
+          <label className="value"> {objUser.user.familyName} </label>
+        </div>
+
+        <div className="contentUser">
+          <label className="key">CONTRASEÑA:</label>
+          <label className="value"> *****</label>
+        </div>
+      </div>
+    )
+};
+
+
+export default DetailProfile;
