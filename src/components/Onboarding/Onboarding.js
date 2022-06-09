d6076c3b5eb6240c7c4e6b5045d51ac0f0f8a4ce
SusanaChen03 <you@example.com>
2022-06-09 11:26:05 +0200

    Update styles Css Onboarding

--- src/components/Onboarding/Onboarding.js
+++ src/components/Onboarding/Onboarding.js
@@ -6,33 +6,30 @@ import icon4 from "../../IMG/streamline-icon-boombox@250x250 1.png";
 import { useNavigate } from "react-router-dom";
 
 const Onboarding = () => {
-
   const navigate = useNavigate();
 
-  const buttonHandler = () =>{
-    navigate('/register')
+  const buttonHandler = () => {
+    navigate("/register");
   };
 
-   return (
+  return (
     <div>
       <div className="container">
-      
         <div className="left">
           <img src={logo} className="appLogo" alt="logo" />
           <p className="p1">Colabora en familia</p> <br />
           <p className="p2">Mejora en familia</p> <br />
           <p className="p3">Crece en familia</p>
         </div>
- 
+
         <div class="right">
           <p class="p4">
-          !Nadie conoce a los tuyos mejor que tú! <br/>
+            !Nadie conoce a los tuyos mejor que tú! <br />
             Y confiamos en tu juicio. <br /> Juntos, encontraremos la manera
             <br /> de modificar los hábitos y adaptarlos
             <br /> a vuestro estilo de vida
           </p>
         </div>
-        
       </div>
 
       <div className="body">
@@ -41,21 +38,30 @@ const Onboarding = () => {
         </div>
         <h1>LLEGARÁS A CONSEGUIR</h1>
         <br />
-        <div class="center">
-          <h3>Conectar mejor con los tuyos y los que te rodean</h3>
-        </div>
-        <br />
-        <div class="middle">
+
+        <div className="Part2">
           <img src={icon3} alt="iconMvp" />
-          <h3>Haz que tu familia se sientan cercanos a ti</h3>
-        </div>
-        <div class="buttom">
-          <h3>Levantarte por las mañanas lleno de energía y alegría</h3>
+          <div class="phases">
+            <p className="explain">
+              Conectar mejor con los tuyos y los que te rodean
+            </p>
+            <p className="explain">
+              Haz que tu familia se sientan cercanos a ti
+            </p>
+            <p className="explain">
+              Levantarte por las mañanas lleno de energía y alegría
+            </p>
+          </div>
           <img src={icon4} alt="iconboom" />
         </div>
-        <input className="discover" type="button" value="Descúbrelo" onClick={buttonHandler}/>
+
+        <input
+          className="discover"
+          type="button"
+          value="Descúbrelo"
+          onClick={buttonHandler}
+        />
       </div>
-      
     </div>
   );
 };
