0a2596d557596bca7ba728b882847d37b26150f5
SusanaChen03 <you@example.com>
2022-06-10 17:21:02 +0200

    Fit miniCard with app style

--- src/components/MiniCardChallenger/MiniCardChallenger.js
+++ src/components/MiniCardChallenger/MiniCardChallenger.js
@@ -1,7 +1,19 @@
 import "./MiniCardChallenger.css";
-import { IoAddCircleOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const MiniCardChallenger = (card) => {
+
+    const navigate = useNavigate();
+
+    const buttonHandler = async () => {
+      try {
+        navigate("/CreateChallenge/"+card.card.id);
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
   return (
       <div className="minicardMini"> 
     <section class="cardMini-section">
@@ -10,15 +22,10 @@ const MiniCardChallenger = (card) => {
                         <div class="flip-cardMini__container">
                             <div class="cardMini-front">
                                 <div class="cardMini-front__tp cardMini-front__tp--city">
-                                 <h3> <IoAddCircleOutline/></h3>   
                                <h2 class="cardMini-front__heading">
                                {card.card.name}
                             </h2>
-                            <p class="cardMini-front__text-price">
-                                Repeticiones:{card.card.repeat}
-                            </p>
                                 </div>
-
                                 <div class="cardMini-front__bt">
                                     <p class="cardMini-front__text-view cardMini-front__text-view--city">
                                         Abrir
@@ -27,7 +34,7 @@ const MiniCardChallenger = (card) => {
                             </div>
                             <div class="cardMini-back">
                            <img
-                            src="https://areajugones.sport.es/wp-content/uploads/2022/01/eternals-2.jpg"
+                            src={card.card.image}
                             class="video__container"
                             alt="cinema"
                         />
@@ -38,12 +45,12 @@ const MiniCardChallenger = (card) => {
                     <div class="inside-page">
                         <div class="inside-page__container">
                             <h3 class="inside-page__heading inside-page__heading--city">
-                            {card.card.name}
+                            Repeticiones:  {card.card.repeat}
                             </h3>
                             <p class="inside-page__text">
                             {card.card.reward}
                             </p>
-                            <a href="#" class="inside-page__btn inside-page__btn--city">Detalles</a>
+                            <a href="#" class="inside-page__btn inside-page__btn--city"  onClick={buttonHandler}>Modificar</a>
                         </div>
                     </div>
                 </div>
