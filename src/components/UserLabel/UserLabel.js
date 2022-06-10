f703fcb8877daed70e4c5ef5ae15f5e53da34c33
SusanaChen03 <you@example.com>
2022-06-10 17:35:51 +0200

    UserLabel: bring the familyMembers

--- src/components/UserLabel/UserLabel.js
+++ src/components/UserLabel/UserLabel.js
@@ -1,11 +1,31 @@
-
 import "./UserLabel.css";
 
 const UserLabel = (allUsers) => {
+  const buttonHandlerUser = (name) => {
+    const btns = document.querySelectorAll("button[ty]");
+    console.log(name);
+
+    [...btns].forEach((btn) => {
+      btn.classList.remove("selected");
+      btn.setAttribute("clicked", "false");
+      if (btn.getAttribute("value") == name) {
+        btn.classList.add("selected");
+        btn.setAttribute("clicked", "true");
+      }
+    });
+  };
   return (
     <div>
-        <button type="button" class="btn btn-outline-danger names">
-            {allUsers.allUsers.name}</button>
+      <button
+        type="button"
+        class="btn btn-outline-danger names"
+        ty="member" 
+        value={allUsers.allUsers.id}
+        clicked="false"
+        onClick={() => buttonHandlerUser(allUsers.allUsers.id)}
+      >
+        {allUsers.allUsers.name}
+      </button>
     </div>
   );
 };
