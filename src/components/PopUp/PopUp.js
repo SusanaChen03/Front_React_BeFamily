9afaff7940749bcc3295fb15db57749a24304a15
SusanaChen03 <you@example.com>
2022-06-10 17:36:53 +0200

    Popup: add css styles

--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -1,14 +1,15 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./PopUp.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const PopUp = () => {
   const popupState = useSelector((state) => state.popup);
 
   return (
     <div>
-      {popupState.visibility && (
-        <div className="popup">
-          <p>{popupState.text}</p>
+      {popupState.visibilidad && (
+        <div class="popup">
+          <p>{popupState.texto}</p>
         </div>
       )}
     </div>
