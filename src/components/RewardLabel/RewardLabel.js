43aba7ed00e31323770ce8d2c567642b82f15584
SusanaChen03 <you@example.com>
2022-06-10 17:36:30 +0200

    RewardLabel: bring all the familyRewads

--- src/components/RewardLabel/RewardLabel.js
+++ src/components/RewardLabel/RewardLabel.js
@@ -1,11 +1,34 @@
-import './RewardLabel.css';
+import "./RewardLabel.css";
 
 const RewardLabel = (allRewards) => {
+  const buttonHandlerUser = (name) => {
+    const btns = document.querySelectorAll("img[ty]");
+    
+     
+    console.log(name);
+
+    [...btns].forEach((btn) => {
+      btn.classList.remove("selectedReward");
+      btn.setAttribute("clicked", "false");
+      if (btn.getAttribute("value") == name) {
+        btn.classList.add("selectedReward");
+        btn.setAttribute("clicked", "true");
+      }
+    });
+  };
   return (
-      
-    <div  >
+    <div>
       <div className="rewardBox">
-        <img className="rewardImage" src={allRewards.allRewards.image} alt="rewardImage" />
+        <img
+          className="rewardImage rewardsel"
+          value={allRewards.allRewards.id}
+          ty="rewads"
+          clicked="false"
+          namereward={allRewards.allRewards.name}
+          onClick={() => buttonHandlerUser(allRewards.allRewards.id)}
+          src={allRewards.allRewards.image}
+          alt="rewardImage"
+        />
         <p className="rewardName">{allRewards.allRewards.name}</p>
       </div>
     </div>
@@ -13,6 +36,3 @@ const RewardLabel = (allRewards) => {
 };
 
 export default RewardLabel;
-
-
-
