c49a3aeff8d341aee5e0fd9ec8aa3b5bfafbe67a
SusanaChen03 <you@example.com>
2022-06-12 11:58:58 +0200

    add more lines in the formulary

--- src/containers/CreateReward/CreateReward.js
+++ src/containers/CreateReward/CreateReward.js
@@ -3,10 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { SHOW_POPUP, URL_LOCAL, HIDDEN_POPUP } from "../../store/typesVar.js";
 import actionCreator from "../../store/actionTypes";
 import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useEffect} from "react";
 
 const CreateReward = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const {id} = useParams();
 
   const formSubmit = async (e) => {
     e.preventDefault();
@@ -15,28 +18,53 @@ const CreateReward = () => {
         name: e.target[0].value,
         image: e.target[1].value,
         description: e.target[2].value,
+        color: e.target[3].value,
+        familyName: sessionStorage.getItem("familyName")
       };
-
-      let postReward = await fetch(URL_LOCAL + "/reward", {
-        method: "POST",
-        body: JSON.stringify(formData),
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + sessionStorage.getItem("token"),
-        },
-      });
-
-      const rewardData = await postReward.json();
-
-      if (rewardData.success === true) {
-        dispatch(
-          actionCreator(SHOW_POPUP, "Se ha creado una nueva recompensa")
-        );
-        alert("The new reward is generated");
-        navigate("/listRewards");
-      } else {
-        alert("reward creation failed:" + rewardData.data);
+      if(id===undefined){
+        let postReward = await fetch(URL_LOCAL + "/reward", {
+          method: "POST",
+          body: JSON.stringify(formData),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + sessionStorage.getItem("token"),
+          },
+        });
+  
+        const rewardData = await postReward.json();
+  
+        if (rewardData.success === true) {
+          dispatch(
+            actionCreator(SHOW_POPUP, "Se ha creado una nueva recompensa")
+          );
+          
+          navigate("/listRewards");
+        } else {
+          alert("reward creation failed:" + rewardData.data);
+        }
+      }else{
+        let postReward = await fetch(URL_LOCAL + "/reward/" + id, {
+          method: "PATCH",
+          body: JSON.stringify(formData),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + sessionStorage.getItem("token"),
+          },
+        });
+  
+        const rewardData = await postReward.json();
+  
+        if (rewardData.success === true) {
+          dispatch(
+            actionCreator(SHOW_POPUP, "Se ha creado una nueva recompensa")
+          );
+          
+          navigate("/listRewards");
+        } else {
+          alert("reward creation failed:" + rewardData.data);
+        }
       }
+      
     } catch (error) {
       dispatch(
         actionCreator(SHOW_POPUP, "No se ha podido crear la recompensa")
@@ -47,33 +75,107 @@ const CreateReward = () => {
     }
   };
 
+  
+  const buttonHandlerDelete = async () => {
+    try {
+     
+      const results = await fetch(URL_LOCAL + "/reward/" + id, {
+        method: "delete",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + sessionStorage.getItem("token"),
+        },
+      });
+      const dataRewards = await results.json();
+      if(dataRewards)
+      { navigate("/listRewards");}
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+
+  const getReward = async (idReward) => {
+    const results = await fetch(URL_LOCAL + "/reward/id/" + idReward, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + sessionStorage.getItem("token"),
+      },
+    });
+
+    const dataRewards = await results.json();
+    if(dataRewards){
+      console.log("name"+dataRewards.name);
+      document.getElementById("rewardName").value=dataRewards.name;
+      document.getElementById("rewardUrl").value=dataRewards.image;
+      document.getElementById("description").value=dataRewards.description;
+    }
+     
+  };
+  useEffect(() => {
+    try {
+     
+      console.log(id);
+      if(id!==undefined){
+        getReward(id);
+        document.getElementById("title").innerText="Edición de la recompensa";
+      }else{
+            
+      }
+     
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  
+
   return (
     <div>
       <div>
-        <h1>Creación de una recompensa</h1>
+        <h1 id='title'>Creación de una recompensa</h1>
 
         <form onSubmit={(e) => formSubmit(e)}>
           <div>
-            <label htmlFor="rewardName">Nombre de la recompensa</label>
-            <input type="text" id="rewardName" name="rewardName" />
+            <label className="labelrw" htmlFor="rewardName">Nombre de la recompensa</label>
+            <input type="text" id="rewardName" name="rewardName" className="resRegister" />
 
-            <label htmlFor="rewardUrl">URL de la recompensa</label>
+            <label className="labelrw" htmlFor="rewardUrl">URL de la recompensa</label>
             <input
               type="text"
               id="rewardUrl"
               name="rewardUrl"
-              placeholder="Buscar una imagen para tu recompensa"
+              placeholder="Buscar una imagen para tu recompensa" className="resRegister"
             />
 
-            <label htmlFor="description">Descripción</label>
-            <input type="text" id="description" name="description" />
+            <label className="labelrw" htmlFor="description">Descripción</label>
+            <input type="text" id="description" name="description" className="resRegister" />
+            <div className="ColorActive">
+            <div  className="input-group divColor">  
+                  <label className="chcolor  input-group-text" htmlFor="repetition" for="color">Color</label>
+                  <select  className="form-select chcolor" id="color" name="color" >
+                      <option value="#F7327E">Rosa</option>
+                      <option value="#4D9BE8" selected>Azul</option>
+                      <option value="#4BE682">Verde</option>
+                      <option value="#A16FC8">Morado</option>
+                  </select>
+              </div>
+              </div>
           </div>
+          <div className="operation">
           <input
             type="submit"
-            value="CREAR RECOMPENSA"
-            className="sendButton"
+            value="Guardar"
+            className="sendButton resRegister btnSave"
           />
+          <button   
+            className="sendButton resRegister btnDelete" onClick={buttonHandlerDelete} 
+          >Eliminar</button>
+          </div>
+           
         </form>
+        
       </div>
     </div>
   );
